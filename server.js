95d6c83fc14cf19231552779aa3b4ad5a6fbd3b2
Cheng Lou <chenglou92@gmail.com>
2014-06-29 22:33:53 -0700

    Update dep to express 4

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 var comments = [{author: 'Pete Hunt', text: 'Hey there!'}];
 
 app.use('/', express.static(__dirname));
-app.use(express.bodyParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/comments.json', function(req, res) {
   res.setHeader('Content-Type', 'application/json');
@@ -18,3 +20,5 @@ app.post('/comments.json', function(req, res) {
 });
 
 app.listen(3000);
+
+console.log('Server started: http://localhost:3000/');
