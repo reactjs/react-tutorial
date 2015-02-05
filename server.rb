b093b3450da81b438a892991e0aa3e6ad554c75f
Leonidez Acosta <acosta.leonidez@gmail.com>
2015-02-04 20:32:22 -0600

    read _comments.json after request received
    The _comments.json file was getting cached and would not update the view when the json was changed.

--- server.rb
+++ server.rb
@@ -11,14 +11,14 @@
 require 'webrick'
 require 'json'
 
-comments = react_version = JSON.parse(File.read('./_comments.json'))
-
 puts 'Server started: http://localhost:3000/'
 
 root = File.expand_path './public'
 server = WEBrick::HTTPServer.new :Port => 3000, :DocumentRoot => root
 
 server.mount_proc '/comments.json' do |req, res|
+  comments = react_version = JSON.parse(File.read('./_comments.json'))
+  
   if req.request_method == 'POST'
     # Assume it's well formed
     comments << req.query
