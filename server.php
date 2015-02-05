094d32cc39b1b857a03487058bfaf41bdafa7b9a
Andy Mathys <andy@mindloop.be>
2015-02-05 21:45:48 +0100

    small refactoring

--- server.php
+++ server.php
@@ -1,25 +1,35 @@
 <?php
-if(isset($_SERVER['argv'][0]) && $_SERVER['argv'][0] === 'server.php') {
+$scriptInvokedFromCli =
+    isset($_SERVER['argv'][0]) && $_SERVER['argv'][0] === 'server.php';
+
+if($scriptInvokedFromCli) {
+    echo 'server listening on port 3000';
     exec('php -S localhost:3000 -t public server.php');
+} else {
+    return routeRequest();
 }
 
-$comments = file_get_contents('_comments.json');
-switch($_SERVER["REQUEST_URI"]) {
-    case '/':
-        echo file_get_contents('./public/index.html');
-        break;
-    case '/comments.json':
-        if($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $commentsDecoded = json_decode($comments, true);
-            $commentsDecoded[] = ['author'  => $_POST['author'], 
-                                  'text'    => $_POST['text']];
+function routeRequest()
+{
+    $comments = file_get_contents('_comments.json');
+    switch($_SERVER["REQUEST_URI"]) {
+        case '/':
+            echo file_get_contents('./public/index.html');
+            break;
+        case '/comments.json':
+            if($_SERVER['REQUEST_METHOD'] === 'POST') {
+                $commentsDecoded = json_decode($comments, true);
+                $commentsDecoded[] = ['author'  => $_POST['author'], 
+                    'text'    => $_POST['text']];
 
-            $comments = json_encode($commentsDecoded);
-            file_put_contents('_comments.json', $comments);
-        } 
-        header('Content-Type: application/json');
-        echo $comments;
-        break;
-    default:
-        return false;
+                $comments = json_encode($commentsDecoded);
+                file_put_contents('_comments.json', $comments);
+            } 
+            header('Content-Type: application/json');
+            echo $comments;
+            break;
+        default:
+            return false;
+    }
 }
+
