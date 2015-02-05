d62a9681ddd63744543c7dbd07d16a5a5f68a15a
Andy Mathys <andy@mindloop.be>
2015-02-05 20:04:41 +0100

    add initial version of the php server

---
+++ server.php
@@ -0,0 +1,21 @@
+<?php
+$comments = file_get_contents('_comments.json');
+switch($_SERVER["REQUEST_URI"]) {
+    case '/':
+        echo file_get_contents('./public/index.html');
+        break;
+    case '/comments.json':
+        if($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $commentsDecoded = json_decode($comments, true);
+            $commentsDecoded[] = ['author'  => $_POST['author'], 
+                                  'text'    => $_POST['text']];
+
+            $comments = json_encode($commentsDecoded);
+            file_put_contents('_comments.json', $comments);
+        } 
+        header('Content-Type: application/json');
+        echo $comments;
+        break;
+    default:
+        return false;
+}
