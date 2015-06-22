83202d3063fdc06976646ecbcfe1cac72d298205
Alexander F Rødseth <rodseth@gmail.com>
2015-06-22 03:27:05 +0200

    Made the Lua sample work like the other samples

--- server.lua
+++ server.lua
@@ -1,22 +1,28 @@
 --
 -- For use with Algernon / Lua
--- 
+--
 -- Project page: https://github.com/xyproto/algernon
 -- Web page: http://algernon.roboticoverlords.org/
 --
 
--- Set the headers
-content("application/javascript")
-setheader("Cache-Control", "no-cache")
+handle("/comments.json", function()
 
--- Use a JSON file for the comments
-comments = JFile("comments.json")
+  -- Set the headers
+  content("application/javascript")
+  setheader("Cache-Control", "no-cache")
 
--- Handle requests
-if method() == "POST" then
-  -- Add the form data table to the JSON document
-  comments:add(ToJSON(formdata()))
-else
-  -- Return the contents of the JSON file
-  print(tostring(comments))
-end
+  -- Use a JSON file for the comments
+  comments = JFile("comments.json")
+
+  -- Handle requests
+  if method() == "POST" then
+    -- Add the form data table to the JSON document
+    comments:add(ToJSON(formdata()))
+  else
+    -- Return the contents of the JSON file
+    print(tostring(comments))
+  end
+
+end)
+
+servedir("/", "public")
