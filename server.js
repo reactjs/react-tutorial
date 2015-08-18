16eff0323b3956574240a5749a833c145d41f3cf
Robby <remmert@rbc.org>
2015-08-18 06:53:27 -0400

    comments.json saving to the right place

--- server.js
+++ server.js
@@ -15,25 +15,26 @@ var path = require('path');
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var rootDirectory = path.join(__dirname, 'public')
 
 app.set('port', (process.env.PORT || 3000));
 
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(rootDirectory));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/comments.json', function(req, res) {
-  fs.readFile('comments.json', function(err, data) {
+  fs.readFile(rootDirectory + '/comments.json', function(err, data) {
     res.setHeader('Cache-Control', 'no-cache');
     res.json(JSON.parse(data));
   });
 });
 
 app.post('/comments.json', function(req, res) {
-  fs.readFile('comments.json', function(err, data) {
+  fs.readFile(rootDirectory + '/comments.json', function(err, data) {
     var comments = JSON.parse(data);
     comments.push(req.body);
-    fs.writeFile('comments.json', JSON.stringify(comments, null, 4), function(err) {
+    fs.writeFile(rootDirectory + '/comments.json', JSON.stringify(comments, null, 4), function(err) {
       res.setHeader('Cache-Control', 'no-cache');
       res.json(comments);
     });
