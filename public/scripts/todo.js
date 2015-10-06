8208b652b8714bbf38e9261904e618ddeb36e266
Gabriel Firmacion <gabrielfirmacion@gmail.com>
2015-10-05 17:21:33 -0700

    add a delete button

--- public/scripts/todo.js
+++ public/scripts/todo.js
@@ -1,104 +1,74 @@
-var Todo = React.createClass({
-  rawMarkup: function() {
-    var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
-    return { __html: rawMarkup };
-  },
+var TaskList = React.createClass({
+    deleteElement:function(){
+    },
 
-  render: function() {
-    return (
-      <div className="todo">
-        <span dangerouslySetInnerHTML={this.rawMarkup()} />
-      </div>
-    );
-  }
+    render: function(){
+
+        var displayTask  = function(task, taskIndex){
+
+            return <div>
+                {task}
+                <button onClick= {this.deleteElement}> Delete </button>
+            </div>;
+        };
+
+        return <div>
+            {this.props.items.map((task, taskIndex) =>
+                <div key={taskIndex}>
+                    {task}
+                    <button onClick={this.props.deleteTask} value={taskIndex}> Complete </button>
+                </div>
+            )}
+        </div>;
+    }
+ });
+
+var TaskApp = React.createClass({
+    getInitialState: function(){
+        return {
+             items: [],
+             task: ''
+        }
+    },
+
+    deleteTask: function(e) {
+        var taskIndex = parseInt(e.target.value, 10);
+        this.setState(state => {
+            state.items.splice(taskIndex, 1);
+            return {items: state.items};
+        });
+    },
+
+    onChange: function(e) {
+        this.setState({ task: e.target.value });
+    },
+
+
+
+    addTask:function (e){
+        this.setState({
+            items: this.state.items.concat([this.state.task]),
+
+            task: ''
+        })
+
+        e.preventDefault();
+    },
+
+    render: function(){
+        return(
+            <div>
+                <h1>My Task </h1>
+                <TaskList items={this.state.items} deleteTask={this.deleteTask} />
+
+                <form onSubmit={this.addTask}>
+                    <input onChange={this.onChange} type="text" value={this.state.task}/>
+                    <button> Add Task </button>
+                </form>
+            </div>
+        );
+    }
 });
 
-var TodoBox = React.createClass({
-  loadTodosFromServer: function() {
-    $.ajax({
-      url: this.props.url,
-      dataType: 'json',
-      cache: false,
-      success: function(data) {
-        this.setState({data: data});
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.error(this.props.url, status, err.toString());
-      }.bind(this)
-    });
-  },
-  handleTodoSubmit: function(todo) {
-    var todos = this.state.data;
-    var newTodos = todos.concat([todo]);
-    this.setState({data: newTodos});
-    $.ajax({
-      url: this.props.url,
-      dataType: 'json',
-      type: 'POST',
-      data: todo,
-      success: function(data) {
-        this.setState({data: data});
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.error(this.props.url, status, err.toString());
-      }.bind(this)
-    });
-  },
-  getInitialState: function() {
-    return {data: []};
-  },
-  componentDidMount: function() {
-    this.loadTodosFromServer();
-    setInterval(this.loadTodosFromServer, this.props.pollInterval);
-  },
-  render: function() {
-    return (
-      <div className="todoBox">
-        <h1>My Todo List</h1>
-        <TodoList data={this.state.data} />
-        <TodoForm onTodoSubmit={this.handleTodoSubmit} />
-      </div>
-    );
-  }
-});
+React.render(<TaskApp />, document.getElementById('content'));
 
-var TodoList = React.createClass({
-  render: function() {
-    var todoNodes = this.props.data.map(function(todo) {
-      return (
-        <Todo>
-          {todo.text}
-        </Todo>
-      );
-    });
-    return (
-      <div className="todoList">
-        {todoNodes}
-      </div>
-    );
-  }
-});
-
-var TodoForm = React.createClass({
-  handleSubmit: function(e) {
-    e.preventDefault();
-
-    var text = React.findDOMNode(this.refs.text).value.trim();
-
-    this.props.onTodoSubmit({text: text});
-    React.findDOMNode(this.refs.text).value = '';
-  },
-  render: function() {
-    return (
-      <form className="todoForm" onSubmit={this.handleSubmit}>
-        <input type="text" placeholder="Add a Todo" ref="text" />
-        <input type="submit" value="Submit" />
-      </form>
-    );
-  }
-});
-
-React.render(
-  <TodoBox url="/api/comments" pollInterval={2000} />,
-  document.getElementById('content')
-);
