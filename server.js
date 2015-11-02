553d7d570028ddca8705a48227e7499a4227d6f2
INOUE Takuya <inouetakuya5@gmail.com>
2015-11-02 23:48:43 +0900

    JSON のインデントサイズを 2 に変更した

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.post('/api/comments', function (req, res) {
   fs.readFile(COMMENTS_FILE, function (err, data) {
     var comments = JSON.parse(data);
     comments.push(req.body);
-    fs.writeFile(COMMENTS_FILE, JSON.stringify(comments, null, 4), function (err) {
+    fs.writeFile(COMMENTS_FILE, JSON.stringify(comments, null, 2), function (err) {
       res.setHeader('Cache-Control', 'no-cache');
       res.json(comments);
     });
