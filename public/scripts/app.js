a91599085579765c5b80ef3702b67dda32993377
INOUE Takuya <inouetakuya5@gmail.com>
2015-11-04 00:05:41 +0900

    JSX をざっくり書いた

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,6 +3,19 @@ var CommentBox = React.createClass({
     return (
       <div className="commentBox">
         <h1>Comments</h1>
+
+        <div className="comment">
+          <h2 className="commentAuthor">
+            My Author
+          </h2>
+          <span>foo</span>
+        </div>
+
+        <form className="commentForm" onSubmit="">
+          <input type="text" placeholder="Your name" ref="author" />
+          <input type="text" placeholder="Say something..." ref="text" />
+          <input type="submit" value="Post" />
+        </form>
       </div>
     );
   }
