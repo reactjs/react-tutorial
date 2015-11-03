bc12d5547a9ac38d43289db88fed5c396701454f
INOUE Takuya <inouetakuya5@gmail.com>
2015-11-04 00:21:28 +0900

    ビューの組み立ては理解した

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,8 +1,62 @@
+var Comment = React.createClass({
+  rawMarkup: function () {
+    var rawMarkup = marked(this.props.children.toString(), { sanitize: true });
+    return { __html: rawMarkup };
+  },
+
+  render: function () {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        <span dangerouslySetInnerHTML={this.rawMarkup()} />
+      </div>
+    );
+  }
+});
+
+var CommentList = React.createClass({
+  render: function () {
+    var commentNodes = this.props.data.map(function (comment, index) {
+      return (
+        // `key` is a React-specific concept and is not mandatory for the
+        // purpose of this tutorial. if you're curious, see more here:
+        // http://facebook.github.io/react/docs/multiple-components.html#dynamic-children
+        <Comment author={comment.author} key={index}>
+          {comment.text}
+        </Comment>
+      );
+    });
+    return (
+      <div className="commentList">
+        {commentNodes}
+      </div>
+    );
+  }
+});
+
 var CommentBox = React.createClass({
+  getInitialState: function () {
+    return {
+      data: [
+        {
+          "author": "Pete Hunt",
+          "text": "Hey there!"
+        },
+        {
+          "author": "Paul O’Shannessy",
+          "text": "React is *great*!"
+        }
+      ]
+    };
+  },
   render: function () {
     return (
       <div className="commentBox">
         <h1>Comments</h1>
+
+        <CommentList data={this.state.data} />
 
         <div className="comment">
           <h2 className="commentAuthor">
