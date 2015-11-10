df7c996da0c4e59df35ba19031e46f9fadff56c0
ramiruhayel <rami@ruhayel.com>
2015-11-10 15:16:19 +0800

    Updated tutorial to use callback refs - React Bug #5427

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -100,20 +100,20 @@ var CommentList = React.createClass({
 var CommentForm = React.createClass({
   handleSubmit: function(e) {
     e.preventDefault();
-    var author = this.refs.author.value.trim();
-    var text = this.refs.text.value.trim();
+    var author = this.authorInput.value.trim();
+    var text = this.textInput.value.trim();
     if (!text || !author) {
       return;
     }
     this.props.onCommentSubmit({author: author, text: text});
-    this.refs.author.value = '';
-    this.refs.text.value = '';
+    this.authorInput.value = '';
+    this.textInput.value = '';
   },
   render: function() {
     return (
       <form className="commentForm" onSubmit={this.handleSubmit}>
-        <input type="text" placeholder="Your name" ref="author" />
-        <input type="text" placeholder="Say something..." ref="text" />
+        <input type="text" placeholder="Your name" ref={(ref) => {this.authorInput = ref}} />
+        <input type="text" placeholder="Say something..." ref={(ref) => {this.textInput = ref}} />
         <input type="submit" value="Post" />
       </form>
     );
