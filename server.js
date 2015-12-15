c600b033ad13773cac9c11a279c3a62d8744f45f
Willson Mock <willson.mock@gmail.com>
2015-12-15 00:30:00 -0800

    Modified server.js so that Cors is allowed in NodeJS server.

--- server.js
+++ server.js
@@ -23,6 +23,11 @@ app.set('port', (process.env.PORT || 3000));
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.get('/api/comments', function(req, res) {
   fs.readFile(COMMENTS_FILE, function(err, data) {
