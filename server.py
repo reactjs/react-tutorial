82424fa6ce90f384e418c54bd9bdc61216e550d8
Andrew Abraham <akabraham@gmail.com>
2016-05-10 15:53:38 -0700

    Remove shadowing of file builtin in server.py. PEP8 formatting

--- server.py
+++ server.py
@@ -16,21 +16,29 @@ from flask import Flask, Response, request
 app = Flask(__name__, static_url_path='', static_folder='public')
 app.add_url_rule('/', 'root', lambda: app.send_static_file('index.html'))
 
+
 @app.route('/api/comments', methods=['GET', 'POST'])
 def comments_handler():
-
-    with open('comments.json', 'r') as file:
-        comments = json.loads(file.read())
+    with open('comments.json', 'r') as f:
+        comments = json.loads(f.read())
 
     if request.method == 'POST':
-        newComment = request.form.to_dict()
-        newComment['id'] = int(time.time() * 1000)
-        comments.append(newComment)
+        new_comment = request.form.to_dict()
+        new_comment['id'] = int(time.time() * 1000)
+        comments.append(new_comment)
 
-        with open('comments.json', 'w') as file:
-            file.write(json.dumps(comments, indent=4, separators=(',', ': ')))
+        with open('comments.json', 'w') as f:
+            f.write(json.dumps(comments, indent=4, separators=(',', ': ')))
 
-    return Response(json.dumps(comments), mimetype='application/json', headers={'Cache-Control': 'no-cache', 'Access-Control-Allow-Origin': '*'})
+    return Response(
+        json.dumps(comments),
+        mimetype='application/json',
+        headers={
+            'Cache-Control': 'no-cache',
+            'Access-Control-Allow-Origin': '*'
+        }
+    )
+
 
 if __name__ == '__main__':
-    app.run(port=int(os.environ.get("PORT",3000)))
+    app.run(port=int(os.environ.get("PORT", 3000)))
